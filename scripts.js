6fbd8a80e21c63cd4c0400bcd513ae29a71f164f
Joana <164516852+xoanaf@users.noreply.github.com>
2024-05-22 09:34:24 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function validarFormulario() {
+      alert('Agradecemos seu contato. O formulário foi enviado!');
+  }
+
+  function menuzinho(){
+    document.getElementById("texto").innerHTML = "";
+    document.getElementById("linkedin").innerHTML = "";
+    document.getElementById("github").innerHTML = "";
+  }
